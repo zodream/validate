d971ba4559192301a2ddc41690ead82df76dc8c9
zx648383079 <648383079@qq.com>
2018-03-17 19:33:17 +0800

    修改

--- src/Rules/PhoneRule.php
+++ src/Rules/PhoneRule.php
@@ -5,6 +5,6 @@ namespace Zodream\Validate\Rules;
 class PhoneRule extends AbstractRegexRule {
 
     protected function getPregFormat() {
-        return '^(0|86|17951)?(13[0-9]|15[012356789]|17[013678]|18[0-9]|19[89]|14[57])[0-9]{8}$';
+        return '/^(0|86|17951)?(13[0-9]|15[012356789]|17[013678]|18[0-9]|19[89]|14[57])[0-9]{8}$/';
     }
 }
